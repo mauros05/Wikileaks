72e1136438caee62a2953745b43b1093e7d8e8a0
Eduardo Gutierrez <emaurogtz@hotmail.es>
2021-10-05 13:09:52 -0500

    Ex3 finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
+
+  get 'articles/new', to: 'articles#new'
+
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  article = Article.create({title: Faker::Movies::StarWars.character, content: Faker::Movies::StarWars.quote })
+end
